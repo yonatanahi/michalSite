001c94c2a59cd33d32d0bded98894599112c834f
yonatanahi <55342512+yonatanahi@users.noreply.github.com>
2020-04-17 12:26:20 +0300

    Adding links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-      Michal's Site
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Michal's Site</h1>
+        <Link to="/">Home</Link>
+        <br/>
+        <Link to="/about">About</Link>
+        <Route path="/about" component={About}/>
+      </div>
+    </Router>
   );
 }
 
